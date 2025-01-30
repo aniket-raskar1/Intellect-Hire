30dfb4851a40b00b70e17377fcd4af261eb96b36
aniket-raskar1 <aniketraskar932@gmail.com>
2025-01-30 14:39:55 +0530

    Added Analytics By vercel to Track analytics of website

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body className={inter.className}>
           <Toaster/>
+          <Analytics />
           {children}</body>
       </html>
     </ClerkProvider>
